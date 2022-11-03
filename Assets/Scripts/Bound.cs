0bc7cf079f49b4a872f33036530059b5b26565c4
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2022-11-03 22:53:08 +0900

    카메라 이동 전체구역 설정

--- Assets/Scripts/Bound.cs
+++ Assets/Scripts/Bound.cs
@@ -10,13 +10,18 @@ public class Bound : MonoBehaviour
     private CameraManager theCamera;
 
     // Start is called before the first frame update
+
+    private void Start() {
+        bound = GetComponent<BoxCollider2D>();
+        theCamera = FindObjectOfType<CameraManager>();
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) //boxCollider에 닿을때 실행되는 내장 함수 is Trigger 체크 해야함
     {
         if(collision.gameObject.name == "Player") //boxCollider에 닿은 객체 이름(player)가 닿았을때 조건문 실행 
         {
-        bound = GetComponent<BoxCollider2D>();
-        theCamera = FindObjectOfType<CameraManager>();
-        theCamera.SetBound(bound);
+            theCamera.SetBound(bound);
         }
     }
 }
