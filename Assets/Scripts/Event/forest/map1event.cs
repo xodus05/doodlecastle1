61d9b258acd77df9fcfcc8bb00d399ab10397cee
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2022-11-17 02:40:49 +0900

    추가중

--- Assets/Scripts/Event/forest/map1event.cs
+++ Assets/Scripts/Event/forest/map1event.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class map1event : MonoBehaviour
 {
+    [SerializeField]
+    public Dialogue Dialogue_1;
+
+    private DialogueManager theDM;
+    private OrderManager theOrder;
+    private ChoiceManager theChoice;
+    private PlayerMove thePlayer;
+
+    private bool flag;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theDM = FindObjectOfType<DialogueManager>();
+        theOrder = FindObjectOfType<OrderManager>();
+        thePlayer = FindObjectOfType<PlayerMove>();
+        theChoice = FindObjectOfType<ChoiceManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (!flag)
+        {
+            flag = true;
+            StartCoroutine(EventCoroutine());
+        }
+    }
+
+    IEnumerator EventCoroutine()
+    {
+        theOrder.PreLoadCharacter(); // 리스트 채우기
+
+        theOrder.NotMove();
+
+        yield return new WaitForSeconds(0.5f);
+
+        theOrder.Move("player", "UP");
+        yield return new WaitUntil(()=>thePlayer.queue.Count == 0);
+
+        theDM.ShowDialogue(Dialogue_1);
+        yield return new WaitUntil(()=>!theDM.talking);
+
+        theOrder.Move();
     }
 }
