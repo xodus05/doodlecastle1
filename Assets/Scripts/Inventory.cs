2485cad4c8ab1da8faf0d1dbc2b57baca20339d2
xodus5 <sudox913@gmail.com>
2023-01-28 01:08:58 +0900

    인벤토리 다시.. 뜯어고치기

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour
 {
     private OrderManager theOrder;
+    public PlayerMove thePlayer;
     //private AudioManager theAudio;
     //public string key_sound;
     //public string enter_sound;
@@ -16,8 +17,9 @@ public class Inventory : MonoBehaviour
     //public string beep_sound;
 
     private InventorySlot[] slots; // 인벤토리 슬롯들
-    //private List<Item> inventoryItemList; // 플레이어가 소지한 아이템 리스트.
-    public Transform tf; //slot 부모객체
+    public List<Item> inventoryItemList; // 플레이어가 소지한 아이템 리스트.
+    public List<Item> inventoryTabList;
+    public Transform tf; //Panel 부모객체
     public GameObject go; // 인벤토리 활성화 비활성화
     public GameObject[] selectedItemImages;
 
@@ -37,14 +39,16 @@ public class Inventory : MonoBehaviour
     {
         theOrder = GetComponent<OrderManager>();
         //theAudio = FindObjectOfType<AudioManager>();
-        //inventoryItemList = new List<Item>();
-        slots = tf.GetComponentsInChildren<InventorySlot>(); 
+        inventoryItemList = new List<Item>();
+        slots = tf.GetComponentsInChildren<InventorySlot>();
+        //inventoryItemList.Add(new Item(thePlayer.haveShovel = true, Item.ItemType.Use));
+
     }
 
     public void ShowItem() // 인벤토리 탭 보여주는 함수
     {
         RemoveSlot();
-        //SelectedItem();
+        SelectedItem();
     }
 
     public void RemoveSlot() // 슬롯값을 InventorySlot에서 처럼 없애주는 함수
@@ -57,27 +61,27 @@ public class Inventory : MonoBehaviour
         }
     }
 
-/*    public void SelectedItem()
+    public void SelectedItem()
     {
         StopAllCoroutines();
         Color color = selectedItemImages[selectedItem].GetComponent<Image>().color;
         color.a = 0f;
-        for(int i = 0; i < selectedItemImages.Length; i++)
+        for (int i = 0; i < selectedItemImages.Length; i++)
         {
             selectedItemImages[i].GetComponent<Image>().color = color;
         }
         StartCoroutine(SelectedItemEffectCoroutine());
-    }*/
+    }
 
-/*    IEnumerator SelectedItemEffectCoroutine()
+    IEnumerator SelectedItemEffectCoroutine()
     {
         while (itemActivated)
         {
             Color color = selectedItemImages[0].GetComponent<Image>().color;
-            while(color.a < 0.5f)
+            while (color.a < 0.5f)
             {
                 color.a += 0.03f;
-                selectedItemImages[selectedItem].GetComponent <Image>().color = color;
+                selectedItemImages[selectedItem].GetComponent<Image>().color = color;
                 yield return waitTime;
             }
             while (color.a < 0f)
@@ -87,12 +91,13 @@ public class Inventory : MonoBehaviour
                 yield return waitTime;
             }
         }
-    }*/
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(!stopKeyInput)
+        
+        if (!stopKeyInput)
         {
             if (Input.GetKeyDown(KeyCode.Tab))
             {
@@ -109,8 +114,8 @@ public class Inventory : MonoBehaviour
                 }
                 else
                 {
-                    //StopAllCoroutines();
-                    go.SetActive(false);
+                    StopAllCoroutines();
+                    go.SetActive(true);
                     itemActivated = false;
                     theOrder.Move();
                     ShowItem();
