50ff78523087ed0bfd015a012a996752f98298b0
chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-03-14 15:13:58 +0900

    추가중

--- Assets/Scripts/Event/forest/campEvent.cs
+++ Assets/Scripts/Event/forest/campEvent.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class trashcanEvent : MonoBehaviour
+public class campEvent : MonoBehaviour
 {
+
     private Inventory inventory;
 
     public Dialogue dialogue_1;
     public Dialogue dialogue_2;
+    public Choice choice_1;
 
+    private FadeManager theFade;
     private DialogueManager theDM;
+    private ChoiceManager theChoice;
     private OrderManager theOrder;
-
-    BoxCollider2D boxCollider;
 
     private bool flag;
 
@@ -20,32 +22,44 @@ public class trashcanEvent : MonoBehaviour
     void Start()
     {
         theDM = FindObjectOfType<DialogueManager>();
+        theChoice = FindObjectOfType<ChoiceManager>();
         theOrder = FindObjectOfType<OrderManager>();
-        boxCollider = GetComponent<BoxCollider2D>();
         inventory = FindObjectOfType<Inventory>();
+        theFade = FindObjectOfType<FadeManager>();
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-            if (!flag && Input.GetKey(KeyCode.Z))
-            {
-                flag = true;
-                StartCoroutine(EventCoroutine());
-            }
+        if (!flag && Input.GetKey(KeyCode.Z))
+        {
+            flag = true;
+            StartCoroutine(EventCoroutine());
+        }
     }
 
     IEnumerator EventCoroutine()
     {
         theOrder.NotMove();
         yield return new WaitForSeconds(0.1f);
-        if(inventory.doing("ë¼ì´í„°")) {
+        if (inventory.haveItem("¶óÀÌÅÍ"))
+        {
             theDM.ShowDialogue(dialogue_2);
-            inventory.inventoryItemList.Add(new Item(5003, "ë¼ì´í„°", Item.ItemType.Use));
-            yield return new WaitUntil(()=>!theDM.talking);
+            yield return new WaitUntil(() => !theDM.talking);
+            theChoice.ShowChoice(choice_1);
+            switch (theChoice.GetResult())
+            {
+                case 0 :
+                    // ºÒÅ¸´Â °Å µîÀå
+                    theFade.Flash(1f);
+                    yield return new WaitUntil(() => !theDM.talking);
+                    break;
+               
+            }
         }
-        else {
+        else
+        {
             theDM.ShowDialogue(dialogue_1);
-            yield return new WaitUntil(()=>!theDM.talking);
+            yield return new WaitUntil(() => !theDM.talking);
             yield return new WaitForSeconds(0.1f);
             flag = false;
         }
