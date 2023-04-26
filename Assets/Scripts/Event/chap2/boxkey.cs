320c89f415acf9d85c5498391292540146a8d3b5
xodus5 <sudox913@gmail.com>
2023-04-26 19:41:30 +0900

    사다리 중복 방지

--- Assets/Scripts/Event/chap2/boxkey.cs
+++ Assets/Scripts/Event/chap2/boxkey.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TextCore.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -18,6 +19,7 @@ public class boxkey : MonoBehaviour
     private OrderManager theOrder;
     private ChoiceManager theChoice;
     private Inventory inventory;
+    private string panelKey = "PanelState"; // PlayerPrefs에 저장할 키값
 
     BoxCollider2D boxCollider;
 
@@ -36,7 +38,12 @@ public class boxkey : MonoBehaviour
         inventory = FindObjectOfType<Inventory>();
         theChoice = FindObjectOfType<ChoiceManager>();
         //inventory.inventoryItemList.Add(new Item(5005, "사다리", Item.ItemType.Use));
-        if (inventory.haveItem("사다리")) Panel.SetActive(true);
+        if (inventory.haveItem("사다리"))
+        {
+            Panel.SetActive(true);
+            Panel1.SetActive(isOpen);
+            Panel.SetActive(!isOpen);
+        }
     }
 
     void Update()
@@ -63,7 +70,7 @@ public class boxkey : MonoBehaviour
         theOrder.PreLoadCharacter(); // 리스트 채우기
         theOrder.NotMove();
         //yield return new WaitUntil(() => thePlayer.queue.Count == 0);
-        if (inventory.haveItem("사다리"))
+        if (inventory.haveItem("사다리") && inventory.haveItem("상자열쇠") == false)
         {
             theDM.ShowDialogue(dialogue_1);
             yield return new WaitUntil(() => !theDM.talking);
@@ -73,18 +80,24 @@ public class boxkey : MonoBehaviour
             switch (theChoice.GetResult())
             {
                 case 0:
-                    Panel1.SetActive(true);
-                    theDM.ShowDialogue(dialogue_2);
-                    yield return new WaitUntil(() => !theDM.talking);
-                    inventory.inventoryItemList.Add(new Item(5006, "상자열쇠", Item.ItemType.Use));
+                    if(inventory.haveItem("상자열쇠") == false)
+                    {
+                        Panel1.SetActive(true);
+                        theDM.ShowDialogue(dialogue_2);
+                        yield return new WaitUntil(() => !theDM.talking);
+                        inventory.inventoryItemList.Add(new Item(5006, "상자열쇠", Item.ItemType.Use));
+                        Panel.SetActive(false);
+                        isOpen = false;
+                    }
                     break;
                 case 1:
                     flag = false;
                     break;
+      
             }
         }
-        Panel.SetActive(false);
-        Panel1.SetActive(true);
+        isOpen = true;
+        flag = false;
         theOrder.Move();
     }
 }
