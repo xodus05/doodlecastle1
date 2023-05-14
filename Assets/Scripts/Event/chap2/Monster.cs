3014db967b6d38d1998a5224463b8866ec1b0431
xodus5 <sudox913@gmail.com>
2023-05-15 01:18:54 +0900

    구현중
    살려주세요

--- Assets/Scripts/Event/chap2/Monster.cs
+++ Assets/Scripts/Event/chap2/Monster.cs
@@ -7,62 +7,12 @@ public class Monster : MonoBehaviour
 {
     private Inventory inventory;
 
+    public Vector2 startPosition;
+
     public static Monster instance;
 
-    #region Singleton
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            DontDestroyOnLoad(this.gameObject);
-            instance = this;
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
-    }
-    #endregion Singleton
+    public GameObject monster;
 
-    Rigidbody2D rb;
-    Transform target;
-    public GameObject Panel;
-
-    private AudioManager theAudio;
-
-    [SerializeField][Range(1f, 500f)] float moveSpeed = 300f;
-
-    [SerializeField][Range(0f, 3f)] float contactDistance = 1f;
-
-    bool follow = false;
-
-    void Start()
-    {
-        inventory = FindObjectOfType<Inventory>();
-        rb = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
-
-    void Update()
-    {
-        FollowTarget();
-    }
-
-    void FollowTarget()
-    {
-        if (Vector2.Distance(transform.position, target.position) > contactDistance && follow)
-        {
-            Panel.transform.position = Vector2.MoveTowards(Panel.transform.position, target.position, moveSpeed * Time.deltaTime);
-        }
-        else
-        {
-            if (follow)
-            {
-                Panel.transform.position = Vector2.MoveTowards(Panel.transform.position, target.position, (moveSpeed) * Time.deltaTime);
-            }
-            //rb.velocity = Vector2.zero;
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
